e70145b3d9c434500121ffed2037f5c438611427
pradeepnegi-tesco <pradeep.negi@equalexperts.com>
2024-05-13 16:27:09 +0530

    basic setup completed

---
+++ models/example/staging/stg_ecommerce__orders.sql
@@ -0,0 +1,22 @@
+with source as (
+    select * from {{source('thelook_ecommerce','orders')}}
+)
+
+select 
+order_id,	
+user_id,	
+status,	
+gender,	
+created_at,	
+returned_at,	
+shipped_at,	
+delivered_at,	
+num_of_item as num_items_orderesd
+from 
+source
+
+
+
+
+
+

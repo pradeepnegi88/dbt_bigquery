32bec007d2af65a71a02aa163cb415de1abcdf0b
pradeepnegi-tesco <pradeep.negi@equalexperts.com>
2024-05-16 16:09:52 +0530

    snapshot added

---
+++ snapshots/snapshot__distribution_centers.sql
@@ -0,0 +1,14 @@
+{% snapshot snapshot__distribution_centers %}
+
+{{
+   config(
+       target_schema='dbt_snapshot',
+       unique_key='id',
+       strategy='check',
+       check_cols=['name','latitude','longitude']
+   )
+}}
+
+select id,name,latitude,longitude from {{ source('thelook_ecommerce', 'distribution_centers') }}
+
+{% endsnapshot %}
